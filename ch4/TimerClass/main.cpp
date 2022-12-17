81b7e37fe1cd39fda0afd5b49436d9026eca6322
Yiru Liu <yiru.liu05@gmail.com>
2022-12-16 21:17:27 -0500

    Finish ch4

--- ch4/TimerClass/main.cpp
+++ ch4/TimerClass/main.cpp
@@ -1,11 +1,55 @@
 #include <sys/time.h>
 #include <cstdio>
+#include <unistd.h>
+
+struct TimerClass {
+    explicit TimerClass(const char *name)
+            : name{name} {
+        struct timeval current_time;
+        gettimeofday(&current_time, nullptr);
+        timestamp = current_time.tv_sec;
+    }
+
+    TimerClass(TimerClass& other) = default;
+    TimerClass& operator=(const TimerClass& other) = default;
+
+    TimerClass(TimerClass&& other) noexcept : timestamp { other.timestamp }, name { other.name } {
+        other.timestamp = 0;
+        other.name = nullptr;
+    }
+    TimerClass& operator=(TimerClass&& other) noexcept {
+        if(this == &other) return *this;
+        timestamp = other.timestamp;
+        name = other.name;
+        other.timestamp = 0;
+        other.name = nullptr;
+        return *this;
+    }
+
+    ~TimerClass() {
+        if(timestamp != 0 && name != nullptr) {
+            struct timeval current_time;
+            gettimeofday(&current_time, nullptr);
+            time_t difference = current_time.tv_sec - timestamp;
+            printf("%s: %lld\n", name, difference);
+        }
+    }
+
+private:
+    time_t timestamp{};
+    const char *name{};
+};
 
 int main() {
-    struct timeval current_time;
-    gettimeofday(&current_time, nullptr);
-    printf("seconds : %ld\nmicro seconds : %ld",
-           current_time.tv_sec, current_time.tv_usec);
+    TimerClass a{"First Timer"};
+
+    sleep(2);
+
+    TimerClass b{"Second Timer"};
+    TimerClass c{ a };
+    TimerClass d = b;
+
+    sleep(2);
 
     return 0;
 }
